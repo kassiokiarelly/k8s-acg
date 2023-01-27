46554e2118a0daccd7c3f2e498123bfa1672591b
Kassio Kiarelly <kassiokiarelly@gmail.com>
2023-01-27 16:28:26 +0000

    Using containerd.io and fixed version of k8s 1.24.10

---
+++ init-k8s.sh
@@ -0,0 +1,1 @@
+sudo kubeadm init --pod-network-cidr 192.168.0.0/16
--- install-k8s.sh
+++ install-k8s.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-cat kernel-modules.conf | tee /etc/modules-load.d/containerd.conf | xargs modprobe
-cat kubernetes-cri.conf | tee /etc/sysctl.d/99-kubernetes-cri.conf
-sysctl --system
-apt-get update && apt-get install -y containerd curl apt-transport-https
-mkdir -p /etc/containerd
-containerd config default | tee /etc/containerd/config.toml
-systemctl restart containerd
-swapoff -a
+sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg lsb-release apt-transport-https
 
+# containerd.io
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# k8s
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
-apt-mark hold kubelet kubeadm kubectl
+cat kernel-modules.conf | sudo tee /etc/modules-load.d/containerd.conf | sudo xargs modprobe
+cat kubernetes-cri.conf | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+sudo sysctl --system
+
+sudo apt-get update && sudo apt-get install -y containerd.io 
+sudo mkdir -p /etc/containerd
+sudo systemctl restart containerd
+sudo swapoff -a
+
+sudo apt-get install -y kubelet=1.24.10-00 kubeadm=1.24.10-00 kubectl=1.24.10-00
+sudo apt-mark hold kubelet kubeadm kubectl
