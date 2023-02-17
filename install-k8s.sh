150b13a2fac4ce35cb3ae679f38bdcac79645b3a
Kássio <kassiokiarelly@gmail.com>
2023-02-17 17:11:27 +0100

    Changing how we add google cloud keys

--- install-k8s.sh
+++ install-k8s.sh
@@ -6,8 +6,8 @@ sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg lsb-re
 curl -fsSL https://get.docker.com | sudo bash
 
 # k8s
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 cat kernel-modules.conf | sudo tee /etc/modules-load.d/containerd.conf | sudo xargs modprobe
 cat kubernetes-cri.conf | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
