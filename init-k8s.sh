8c6d8b1cd3a9ea176a1be5b6bfd7ecf594a7b70a
Kassio Kiarelly <kassiokiarelly@gmail.com>
2023-02-19 18:07:42 +0100

    Enable SystemdCgroup and other script improvements

--- init-k8s.sh
+++ init-k8s.sh
@@ -1,4 +1,11 @@
-sudo kubeadm init --pod-network-cidr 10.144.0.0/16
+sudo kubeadm init --pod-network-cidr 10.244.0.0/16
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+#calico
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml -O
+kubectl apply -f calico.yml
+
+#generate join command
+kubeadm token create --print-join-command > joincmd.txt
--- install-k8s.sh
+++ install-k8s.sh
@@ -5,17 +5,30 @@ sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg lsb-re
 # containerd.io
 curl -fsSL https://get.docker.com | sudo bash
 
+touch containerd.conf
+echo "overlay" > tee containerd.conf
+echo "br_netfilter" >> tee containerd.conf
+sudo mv containerdd.conf /etc/modules-load.d/
+cat /etc/modules-load.d/containerd.conf | sudo xargs modprobe
+
+touch 99-kubernetes-cri.conf
+echo "net.bridge.bridge-nf-call-iptables  = 1" >  tee 99-kubernetes-cri.conf
+echo "net.bridge.bridge-nf-call-ip6tables = 1" >> tee 99-kubernetes-cri.conf
+echo "net.ipv4.ip_forward                 = 1" >> tee 99-kubernetes-cri.conf
+sudo mv 99-kubernetes-cri.conf /etc/sysctl.d/
+sudo sysctl --system
+
+sudo mv /etc/containerd/config.toml /etc/containerd/config.toml.bkp
+containerd config default > config.toml
+sudo sed -i "s/SystemdCgroup = false/SystemdCgroup = true/" config.toml
+sudo mv config.toml /etc/containerd
+sudo systemctl restart containerd
+sudo swapoff -a
+
 # k8s
 sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-cat kernel-modules.conf | sudo tee /etc/modules-load.d/containerd.conf | sudo xargs modprobe
-cat kubernetes-cri.conf | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
-sudo sysctl --system
-
-sudo mv /etc/containerd/config.toml /etc/containerd/config.toml.bkp
-sudo systemctl restart containerd
-sudo swapoff -a
-
+sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
