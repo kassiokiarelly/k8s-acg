b4dd55f5428911d8ba5a37f6df72f6878cfa45ae
Kássio <kassiokiarelly@gmail.com>
2023-02-19 11:57:35 +0100

    Change pod network CIDR

--- init-k8s.sh
+++ init-k8s.sh
@@ -1,4 +1,4 @@
-sudo kubeadm init --pod-network-cidr 192.168.0.0/16
+sudo kubeadm init --pod-network-cidr 10.144.0.0/16
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
